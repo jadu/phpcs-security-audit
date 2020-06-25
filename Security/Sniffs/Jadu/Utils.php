a41c57461e9ca4d4080e0a301c012bd1d0589c68
Sriram Venkateswaran <sriram.venkateswaran@jadu.net>
2020-06-25 15:04:40 +0100

    Added Jadu specific xss mitigation functions

--- Security/Sniffs/Jadu/Utils.php
+++ Security/Sniffs/Jadu/Utils.php
@@ -15,6 +15,26 @@ class Utils extends BaseUtils
     * @return array(String) returns the array of functions
     */
     public static function getXSSMitigationFunctions() {
-        return ['encodeHtml', 'encodeHTML', 'htmlentities', 'htmlspecialchars'];
+        return ['encodeHtml', 'encodeHTML', 'intval'];
+    }
+
+    /**
+    * Verify that a function is a XSS mitigation
+    * By default this function will return TRUE even if a normal PHP mitigation function is used,
+    * because it's considered a bad practice to do otherwise; see second param $isparent
+    *
+    * @param $var   The variable containing the function string
+    * @param Boolean    bool set to TRUE if we check for the parent's functions, default FALSE
+    * @return Boolean   returns TRUE if it's a XSS mitigation function, FALSE otherwise
+    */
+    public static function is_XSS_mitigation($var, $isparent=FALSE) {
+        if ($isparent && parent::is_XSS_mitigation($var)) {
+            return TRUE;
+        } else {
+            if (in_array($var,  Utils::getXSSMitigationFunctions())) {
+                return TRUE;
+            }
+        }
+        return FALSE;
     }
 }
